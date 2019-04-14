63cec9fa5186f31ed937da72dcc3f07bf9be728f
aagarw30 <bricatoo19@gmail.com>
2019-04-14 15:05:06 -0400

    CloudChat
    Vaesoft CloudChat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,37 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+import { ChatPage } from '../pages/chat/chat';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+var config = {
+  apiKey: "AIzaSyCD5S_Td0BZo76OtVOVhjAjD1NPk7XYEqE",
+  authDomain: "chatfire-de182.firebaseapp.com",  
+  databaseURL: "https://chatfire-de182.firebaseio.com",
+  projectId: "chatfire-de182",
+  storageBucket: "chatfire-de182.appspot.com",
+  messagingSenderId: "589815166204"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage, 
+    ChatPage, 
+    
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   providers: [
     StatusBar,
